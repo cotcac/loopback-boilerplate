76186946babee0954fb2251c748ae4622c76cd9a
Your Name <you@example.com>
2019-05-17 04:54:07 +0700

    set up produciton env

--- src/application.ts
+++ src/application.ts
@@ -9,12 +9,26 @@ import { RestApplication } from '@loopback/rest';
 import { ServiceMixin } from '@loopback/service-proxy';
 import * as path from 'path';
 import { MySequence } from './sequence';
+import { DbDataSource } from './datasources/db.datasource'; // import DbDataSource
 
 export class GettingStartedApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+
+    // PRODUCTION DATASOURCE
+
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+      const dbDataSource = new DbDataSource({
+        name: 'db',
+        connector: 'mysql',
+        url: process.env.DATABASE_URL
+      });
+      this.bind('datasources.db').to(dbDataSource);
+    }
+
+    // END
 
     // Set up the custom sequence
     this.sequence(MySequence);
